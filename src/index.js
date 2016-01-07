4af5402ee7a2e84b7f46c0c87fc94aa1a28a297b
solsen <spam2steve@gmail.com>
2016-01-06 22:17:27 -0500

    start using bluebird promises

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
+'use strict'
+
 var GoogleSpreadsheet = require('google-spreadsheet')
 var _ = require('lodash-getpath')
+var BPromise = require('bluebird')
+var utils = require('./utils')
 
 // With auth -- read + write
-var creds = require('./google-generated-creds.json')
+var creds = require('../google-generated-creds.json')
 
 // spreadsheet key is the long id in the sheets URL
 var my_sheet = new GoogleSpreadsheet('1fyGsYhinmTRNpJyw_uVDpI3wYmWz9FXIYgR2DuobZ_w')
 // https://docs.google.com/spreadsheets/d/1fyGsYhinmTRNpJyw_uVDpI3wYmWz9FXIYgR2DuobZ_w/edit
 
-my_sheet.useServiceAccountAuth(creds, function (err) {
+var getInfo_p = BPromise.promisify(my_sheet.getInfo)
+
+my_sheet.useServiceAccountAuth(creds, err => {
   if (err) { }
-  // getInfo returns info about the sheet and an array or "worksheet" objects
-  my_sheet.getInfo(function (err, sheet_info) {
-    if (err) { }
+
+  getInfo_p()
+  .then(sheet_info => {
     console.log(sheet_info.title + ' is loaded')
-    // console.log(JSON.stringify(sheet_info))
+    console.log(JSON.stringify(sheet_info))
     // use worksheet object if you want to stop using the # in your calls
     var sheet1 = sheet_info.worksheets[0]
+    console.log(utils.getColumnData(sheet1))
 
     // # is worksheet id - IDs start at 1
     // my_sheet.getRows(1, function(err, row_data) {
     //   console.log('pulled in ' + row_data.length + ' rows')
     // })
 
-    sheet1.getRows(function (err, rows) {
+    sheet1.getRows((err, rows) => {
       if (err) { }
       console.log(rows.length)
       rows[0].Name = 'A' + Date.now()
@@ -38,7 +45,7 @@ my_sheet.useServiceAccountAuth(creds, function (err) {
       start: 1,       // start index
       num: 100,         // number of rows to pull
       orderby: 'name'  // column to order results by
-    }, function (err, row_data) {
+    }, (err, row_data) => {
       if (err) { }
       console.log('names', _.getPath(row_data, '[].name'))
     })
---
+++ src/utils.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function getColumnData (xml) {
+  return [ 'name' ]
+}
+
+module.exports = {
+  getColumnData
+}
